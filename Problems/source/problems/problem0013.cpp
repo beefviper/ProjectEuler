b3ba7148ae719812e5abafad1ab41ae9d08cde05
beefviper <beef_viper@hotmail.com>
2026-09-14 01:28:55 -0400

    added answer to consturctor, verifed solved

---
+++ Problems/source/problems/problem0013.cpp
@@ -0,0 +1,37 @@
+// problem0013.cpp
+// Project Euler Problems
+// author: beefviper
+// date: Sept 14, 2026
+
+#include "problems.h"
+
+/*
+Problem 13: Large sum
+
+Work out the first ten digits of the sum of the following
+one-hundred 50-digit numbers.
+
+Answer: 5537376230
+*/
+
+
+uint64_t problem13()
+{
+	uint64_t result{ 0 };
+	std::string numbersFileName = "data/numbers.txt";
+	std::vector<std::string> numbers = loadNumbers(numbersFileName);
+	std::string total(50, '0');
+	
+	for (auto& number : numbers) {
+        print << number << std::endl;
+		total = addLargeNumbers(total, number);
+	}
+
+    print << "The total is " << total << "." << std::endl;
+
+	//result = std::stol(total);
+
+	return result;
+}
+
+static const ProblemRegistrar problem13_registrar{ Problem{ 13, problem13, 5537376230 } };
